025ddf34d061689e33a73333e60ab9bb96d9dda2
Dinis Pereira <diniscoutop@gmail.com>
2022-03-11 14:33:03 +0000

    portraits

--- screen/eventVideo/video1.js
+++ screen/eventVideo/video1.js
@@ -73,7 +73,7 @@ const Video1 = () => {
         
         <Image 
         style={{ width: 80, height:100, borderRadius: 12,}}
-        source={require('../../assets/roberto.png') }/> 
+        source={require('../../assets/robertoSousa.png') }/> 
         
         
         <View>
