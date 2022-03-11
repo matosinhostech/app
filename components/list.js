6437e23cbe6a7174ec71f10945d09d55d0a5da3f
Dinis Pereira <diniscoutop@gmail.com>
2022-03-11 14:39:53 +0000

    video's ID update

--- components/list.js
+++ components/list.js
@@ -6,7 +6,7 @@ export const list = [
         date: "February 10th 2022",
         lenght: "17:24",
         description: "How to mix crypto with everyday payments? Roberto Sousa, Head of Engineering at Mode, gives us an insight into how to merge crypto into payments and a breakdown of how to design it to scale and all the challenges associated with it.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "676381927"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const list = [
         date: "February 10th 2022",
         lenght: "19:29",
         description: "Why contribute to open source? Tiago Barbosa, Specialist Solutions Architect at AWS and streamer at the AWS Twitch channel Talk Dev to me, showcases the power and the motivations to do open source projects.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "676380612"
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ export const list = [
         date: "November 26th 2021",
         lenght: "21:46",
         description: "In the Basecone business, one of the most concerning problems is to accurately translate the information present in an invoice or receipt to a human-readable text in the correct format. During the last year, the data science team has been working on algorithms to solve this.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "652798237"
     },
     {
         id: 4,
@@ -33,7 +33,7 @@ export const list = [
         date: "November 26th 2021",
         lenght: "15:07",
         description: "The why and how we build the design system behind TAIKAI. A design system can be the foundation for SaaS companies and centralized space for single or multiple projects. But what seems to be easy to build, it’s not so easy at all. So, we go share what we learn, what we use, the mistakes we made, and what we think about the future for our design system.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "652189921"
     },
     {
         id: 5,
@@ -42,7 +42,7 @@ export const list = [
         date: "September 30th 2021",
         lenght: "15:44",
         description: "In this talk, Sara Pinto, Senior Data Scientist at Tessian, will clarify what machine learning is and isn't, helping us understand and acknowledge the benefits of this technology in our industry. Sara's background is Computer Science but what she really love is combining Software Engineering with Data. She started working as a Software Engineer but picked interest in Data Science by exploring some ML online courses. After working for 3 years in Portugal as a Data Scientist, she moved to London 3 years ago where she continued to learn everyday on how to solve problems. Her day-to-day is building end-to-end solutions from data analysis & modeling to deployment.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "624915219"
     },
     {
         id: 6,
@@ -51,7 +51,7 @@ export const list = [
         date: "September 30th 2021",
         lenght: "19:07",
         description: "In this talk, seasoned software engineer and HUUB CTO, Luís Ramos, will teach us how good software architecture could make or break the scalability of your project, especially in the Logistics as a Service paradigm. Luís is a Software Engineer with 20 years of professional experience. He has worked in Switzerland, France, Portugal and the UK, and across different industries such as E-commerce, Digital Marketing, Banking, Healthcare and Telco. He is passionate about simplicity, software design and Lean Software Development. In the last 5 years he has been CTO at AdClick, Head of Engineering at Farfetch and, in the last year, CTO at HUUB where is helps build the next generation Logistics platform.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "624919801"
     },
     {
         id: 7,
@@ -60,7 +60,7 @@ export const list = [
         date: "February 26th 2021",
         lenght: "19:44",
         description: "Giddy presents itself as being the very first internal product made by Coletiv Studio, fruit of a whiteboard improvement process that is now a mobile application. André Silva is the CTO of Coletiv and a Software Engineer, with focus on iOS and Elixir development from Porto, Portugal.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "518630526"
     },
     {
         id: 8,
@@ -69,7 +69,7 @@ export const list = [
         date: "June 18th 2021",
         lenght: "21:24",
         description: "This talk is split into two parts. During the first part, Pedro will share some anecdotes of his path from CS graduate to getting into Y Combinator, and how a bunch of random decisions led him there. In the second part, he’ll share some insights about what it is like to be part of YC and what kind of lessons he learnt as a tech person and founder, regarding building your own company. Pedro is one of the founders of Satchel. He has a CS degree from Uni. Minho and has since worked in multiple startups as a developer, eng. manager, and CTO. He loves building tech from the ground up, and thrives especially when hacking things together. He was part of the latest Y Combinator batch (W21) in San Francisco.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "568145996"
     },
     {
         id: 9,
@@ -78,7 +78,7 @@ export const list = [
         date: "April 30th 2021",
         lenght: "21:44",
         description: "In this talk Márcio will share how he used Hasura and all its potential to quickly build an API capable of responding to all the necessary requirements without compromising the quality and the capability of the POC. He'll show you how easy is to create a complete GraphQL API with queries, mutations and subscriptions and how to overtake some common obstacles. Márcio Pinto is a full-stack developer at Significa with a special focus on web solutions (React) and mobile hybrid solutions (React-Native). Originally graduated as a Designer at UBI, Márcio is now a software enthusiastic and a perfect solution seeker.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "544663917"
     },
     {
         id: 10,
@@ -87,7 +87,7 @@ export const list = [
         date: "June 18th 2021",
         lenght: "28:00",
         description: "In this talk Jose will recount how he ended up in California and why eventually returned back to origins. And make the case for you to go abroad as well. While graduating in EE from FEUP in 2011, Jose worked in a couple open-source projects that landed him a job in Silicon Valley. While there, Jose co-founded and exited 2 cybersecurity startups. An entrepreneur at heart, Jose can't let go of writing code and helping create consumer-looking enterprise-grade products. Recently back in Portugal, Jose tries to run a family with his wife, newborn and 2 cats.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "568146045"
     },
     {
         id: 11,
@@ -96,7 +96,7 @@ export const list = [
         date: "April 30th 2021",
         lenght: "20:01",
         description: "José and Nelson will explain how they structured their data, stored, and indexed it in their databases to provide fast visualization and processing tools to the user, while also being able to process it using our neural network-based systems for forecasting and automated anomaly detection. José Ferrão is a software engineer at EQS Digital and PhD Student of Computer Engineering at UA, working on web visualisation tools, computer vision and machine learning. Nelson Campos is a software engineer at EQS Digital, obtained his master’s degree of Robotics and Autonomous Systems at ISEP. Interested in electronics and full-stack software development.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "544665590"
     },
     {
         id: 12,
@@ -105,7 +105,7 @@ export const list = [
         date: "February 26th 2021",
         lenght: "15:55",
         description: "IPFS (InterPlanetary File System) is a protocol, and P2P network for storing and sharing data in a distributed file-sharing system, that can be leveraged to more efficiently store and share large files. It relies on cryptographic hashes that can easily be stored on a blockchain. Francisco Sales is passionate about solving complex problems, and simple ones. Always aspiring for perfection, and thriving to innovate through technology. He's also the CoFounder & Tech Lead at Mosano.",
-        url: "https://www.matosinhos.tech/videos#playing"
+        videoID: "518631682"
     }
     
 ]
