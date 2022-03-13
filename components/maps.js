4b854a1716a7fdff7466ed9bf1bcd9d254ae698f
Gonçalo Lobo <goncalobo@gmail.com>
2022-03-13 16:46:15 +0000

    add partner cards to map markers (#3)

--- components/maps.js
+++ components/maps.js
@@ -1,58 +1,62 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Image, View, Linking } from "react-native";
 import { useState, useRef } from "react";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
+// Contants
+import PARTNERS from '../constants/partners';
 
 const Maps = () => {
   const [mapRegion, setRegion] = useState({
-    latitude: 41.16589541103815, 
+    latitude: 41.16589541103815,
     longitude: -8.660416801879878,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
 
-  const selectedMarker = require('../assets/coletiv.png');
-
   return (
     <>
-        <View style={styles.container}>
-            <MapView
-            style={{ alignSelf: 'stretch', height: '100%' }}
-            region={mapRegion}>
-                
-                <Marker // coletiv
-                        coordinate={{ 
-                        latitude : 41.16132235902691, 
-                        longitude : -8.627314607425252 }}
-                        title="Coletiv"
-                        pinColor='violet'
-                />
-                <Marker // basecone
-                        coordinate={{ 
-                        latitude : 41.15680920857158,  
-                        longitude : -8.630009553960608 }}
-                        title="Basecone"
-                        pinColor='violet'
-                />
-                <Marker // newwork
-                        coordinate={{ 
-                        latitude : 41.17819092495354,
-                        longitude : -8.678966853960608 }}
-                        title="New Work SE"
-                        pinColor='violet'
-                />
-                <Marker // glazed
-                        coordinate={{ 
-                        latitude : 41.176205249730934,  
-                        longitude : -8.686051 }}
-                        title="Glazed"
-                        pinColor='violet'
-                />
-            </MapView>
-        </View>
+      <View style={styles.container}>
+        <MapView
+          style={{ alignSelf: 'stretch', height: '100%' }}
+          region={mapRegion}>
+          {PARTNERS.map(({
+            slug,
+            name,
+            address,
+            latitude,
+            longitude,
+            logo,
+            site,
+          }) => (
+            <Marker
+              key={slug}
+              coordinate={{
+                latitude: latitude,
+                longitude: longitude
+              }}
+              title={name}
+              pinColor='violet'
+              onCalloutPress={() => Linking.openURL(site)}>
+              <MapView.Callout>
+                <View style={styles.partnerCard}>
+                  <Text style={styles.name}>{name}</Text>
+                  <Text style={styles.address}>{address}</Text>
+                  {/* 
+                      Something weird happens that makes impossible to use an Image inside a
+                      MapView.Callout component, so we wrap it in Text component, it works ¯\_(ツ)_/¯
+                      https://github.com/react-native-maps/react-native-maps/issues/2633#issuecomment-524324731
+                  */}
+                  <Text style={styles.logoContainer}>
+                    <Image style={styles.logo} source={logo} />
+                  </Text>
+                </View>
+              </MapView.Callout>
+            </Marker>
+          ))}
+        </MapView>
+      </View>
     </>
   );
 };
@@ -61,6 +65,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  partnerCard: {
+    width: 120,
+    height: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    margin: 0,
+    width: 100,
+    height: 60,
+    textAlign: 'center',
+  },
+  logo: {
+    width: 60,
+    height: 30,
+    margin: 0,
+    resizeMode: 'contain',
+  },
+  name: {
+    marginTop: 10,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  address: {
+    fontSize: 10,
+    textAlign: 'center',
+    marginTop: 5,
+    marginBottom: 5
+  }
 });
 
 export default Maps;
